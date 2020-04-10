0e9ba2f487c62d4c6892870ce4935bc7d07d2b58
manhdkgch18056 <62170109+manhdkgch18056@users.noreply.github.com>
2020-04-10 20:28:29 +0700

    8:24PM 4/10/2020

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 const express = require('express');
 const favicon = require('express-favicon');
 const engines = require('consolidate');
-const server = express();
+const app = express();
 var path = require("path");
 var bodyParser = require("body-parser");
-server.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 var publicDir = require('path').join(__dirname, '/public');
 
 
 
 
 //npm i handlebars consolidate --save
-server.engine('hbs', engines.handlebars);
-server.set('views', './views');
-server.set('view engine', 'hbs');
+app.engine('hbs', engines.handlebars);
+app.set('views', './views');
+app.set('view engine', 'hbs');
 
 
 var indexController = require('./index.js');
 var SanPhamController = require('./SanPham.js');
 
-server.use(favicon(__dirname + '/public/images/logo.png'));
+app.use(favicon(__dirname + '/public/images/logo.png'));
 
-server.use('/index', indexController);
-server.use('/sanpham', SanPhamController);
+app.use('/index', indexController);
+app.use('/sanpham', SanPhamController);
 
-var server = server.listen(8000, function () {
+var server = app.listen(8000, function () {
     console.log("Running at port 8000 ")
 });
