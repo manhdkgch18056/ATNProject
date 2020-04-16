01fc983d5f6fd571c7626f11d2fd3ee1ddfadf71
manhdkgch18056 <62170109+manhdkgch18056@users.noreply.github.com>
2020-04-16 14:53:44 +0700

    2:53PM 4/16/2020 Manh

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
+var http = require('http')
+var port = process.env.PORT || 3000;
 const express = require('express');
 const favicon = require('express-favicon');
 const engines = require('consolidate');
 const app = express();
 var path = require("path");
 var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: false}));
 var publicDir = require('path').join(__dirname, '/public');
-
-//npm i handlebars consolidate --save
-app.engine('hbs', engines.handlebars);
-app.set('views', './views');
-app.set('view engine', 'hbs');
-
 var indexController = require('./index.js');
 var productsController = require('./products.js');
-app.use(favicon(__dirname + '/public/images/favicon.ico'));
+var server = http.createServer((function (request, response) {
 
-app.use('/index', indexController);
-app.use('/products', productsController);
-var server = app.listen(8000, function () {
-    console.log("Running at port 8000 ")
+//npm i handlebars consolidate --save
+    app.engine('hbs', engines.handlebars);
+    app.set('views', './views');
+    app.set('view engine', 'hbs');
+
+    app.use(bodyParser.urlencoded({extended: false}));
+
+    app.use(favicon(__dirname + '/public/images/favicon.ico'));
+
+    app.use('/index', indexController);
+    app.use('/products', productsController);
+
+}));
+
+server.listen(port, () => {
+    console.log(`Server running at port ` + port);
 });
